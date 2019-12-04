53d685b1cff0564e88c967e94fbef1d8077f13f5
Saara-Sheneen <bsse0833@iit.du.ac.bd>
2019-12-05 01:19:59 +0600

    android trial

--- AppAndroid/app/src/main/java/com/example/appandroid/AlertActivity.java
+++ AppAndroid/app/src/main/java/com/example/appandroid/AlertActivity.java
@@ -17,14 +17,32 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class AlertActivity extends AppCompatActivity {
 
     private String userId;
+    private String ip;
     private Button buttonStop;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     private SoundPool soundPool;
     private int sound;
+    private OkHttpClient client;
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final String BASE_URL = "http://172.16.20.107:3000/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +52,25 @@ public class AlertActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         buttonStop = findViewById(R.id.btnStop);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .build();
+        client = new OkHttpClient();
 
-            soundPool = new SoundPool.Builder()
-                    .setMaxStreams(6)
-                    .setAudioAttributes(audioAttributes)
-                    .build();
-        } else {
-            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
-        }
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(6)
+                .setAudioAttributes(audioAttributes)
+                .build();
 
         sound = soundPool.load(this, R.raw.alarm, 1);
 
 
         if (bundle != null) {
 
-            this.userId = bundle.getString("ip");
+            this.userId = bundle.getString("userId");
+            this.ip = bundle.getString("ip");
 
             Toast.makeText(AlertActivity.this, this.userId, Toast.LENGTH_LONG).show();
 
@@ -62,14 +79,12 @@ public class AlertActivity extends AppCompatActivity {
 
             myRef.setValue("active");
 
-            // Read from the database
             myRef.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     String value = dataSnapshot.getValue(String.class);
                     if (value.equals("sleepy")){
                         soundPool.play(sound, 1, 1, 0, 0, 1);
-                        //soundPool.autoPause();
                         myRef.setValue("active");
                     }
                     Toast.makeText(AlertActivity.this, value, Toast.LENGTH_LONG).show();
@@ -81,11 +96,55 @@ public class AlertActivity extends AppCompatActivity {
                     // Failed to read value
                 }
             });
+            doRequest(ip);
         }
 
     }
 
+    private void doRequest(String ip){
+        try {
+            Thread.sleep(10 * 1000);
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println(ip);
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("ip", ip);
+            jsonObject.put("type", "2");
+        } catch (JSONException e) {
+            System.out.println(e.toString());
+        }
+        String jsonString = jsonObject.toString();
+        RequestBody body = RequestBody.create(JSON, jsonString);
+        String url = BASE_URL + "";
+        Request request = new Request.Builder()
+                .header("X-Client-Type", "Android")
+                .url(url)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(AlertActivity.this, "Request Failed Try Again!!!", Toast.LENGTH_LONG)
+                        .show();
+                System.out.println(e.toString());
+                System.out.println("++++++++++++++++++++++++++++");
+            }
 
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String res;
+                try {
+                    res = response.body().toString();
+                    System.out.println(res);
+                } catch (NullPointerException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
     public void onStopClick(View view){
         soundPool.play(sound, 1, 1, 0, 0, 1);
